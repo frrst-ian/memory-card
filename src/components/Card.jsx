56bc2e1dc7f8babbd5dde8844962dbe5b26972c6
frrst-ian <frrstxd@proton.me>
2024-10-14 16:51:06 +0800

    feat: Fetch the first data using futurama API
    Add a function to fetch the data from futurama API and console.log it

---
+++ src/components/Card.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from "react";
+
+function Card() {
+  const [characters, setCharacters] = useState([]);
+
+  const fetchCharacters = async () => {
+    try {
+      const response = await fetch(
+        "https://api.sampleapis.com/futurama/characters"
+      );
+      const data = await response.json();
+      const simplifiedData = data.map((character) => ({
+        name: character.name.first + " " + character.name.last,
+        image: character.images.main,
+        sayings: character.sayings[0] || "No saying",
+      }));
+      console.log("First character:", data[0]);
+      setCharacters(simplifiedData);
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+    }
+  };
+  useEffect(() => {
+    fetchCharacters();
+  }, []);
+}
+
+export default Card;
