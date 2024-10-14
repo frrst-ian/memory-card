7b7e5c329067577a21a54bbc53adb195871415b2
frrst-ian <frrstxd@proton.me>
2024-10-14 17:48:01 +0800

    feat: Add name , saying , image
    Add the character information i.e name, saying ,image

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,6 +1,15 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from "react";
 
-const CharacterCard = ({ name, image, saying }) => {};
+const CharacterCard = ({ name, image, saying }) => {
+  return (
+    <div className="card-container">
+      <img src={image} alt={name} />
+      <p>{name}</p>
+      <p>{saying}</p>
+    </div>
+  );
+};
 
-export {CharacterCard};
+export { CharacterCard };
